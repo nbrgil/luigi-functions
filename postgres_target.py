8f7bdc7eaba8c27dd5013b52ae0ad3ff97299e0f
Rodrigo L. Gil <rodrigo.lazarini.gil@gmail.com>
2018-02-12 10:45:00 -0200

    Force use and multiple execution

--- postgres_target.py
+++ postgres_target.py
@@ -1,59 +1,30 @@
 from luigi import Target
-import os
+from sqlalchemy.engine import Engine
 
 
 class PostgresCountTarget(Target):
-    def __init__(
-            self, postgres_client: PostgresClient = None, table_name: str = None, sql_filter: str = None,
-            force: bool = False, schema: str = 'public'):
-        self.postgres_client = postgres_client
-        self.table_name = table_name
-        self.schema = schema
-        self.sql_filter = sql_filter
-        self.force = force
+	def __init__(
+			self,
+			sqlalchemy_engine,
+			table_name: str = None,
+			sql_filter: str = None,
+			schema: str = "public"
+	):
+		self.sqlalchemy_engine = sqlalchemy_engine  # type: Engine
+		self.table_name = table_name
+		self.sql_filter = sql_filter
+		self.schema = schema
 
-    def exists(self):
-        if self.force or os.getenv("LUIGIFORCE") == "True":
-            return False
+	def exists(self):
+		conn = self.sqlalchemy_engine.connect()
 
-        conn = self.postgres_client.get_conn_engine().connect()
+		try:
+			sql = "SELECT count(*) FROM {}.{} {}".format(
+				self.schema, self.table_name, self.sql_filter)
+			count = conn.execute(sql).scalar()
 
-        try:
-            sql = "SELECT count(*) FROM {}.{} {}".format(
-                self.schema, self.table_name, self.sql_filter)
-            count = conn.execute(sql).scalar()
+			return count > 0
 
-            return count > 0
-
-        finally:
-            if conn.closed:
-                conn.close()
-
-
-class PostgresLogTarget(Target):
-    def __init__(
-            self, postgres_client: PostgresClient = None, table_name: str = None, sql_filter: str = None,
-            force: bool = False, schema: str = 'zapvivareal'):
-        self.postgres_client = postgres_client
-        self.table_name = table_name
-        self.schema = schema
-        self.sql_filter = sql_filter
-        self.force = force
-
-    def exists(self):
-        if self.force or os.getenv("LUIGIFORCE") == "True":
-            return False
-
-        conn = self.postgres_client.get_conn_engine().connect()
-
-        try:
-            count = conn.execute(
-                "SELECT count(*) FROM {}.{} {}".format(
-                    self.schema, self.table_name, self.sql_filter)
-            ).scalar()
-
-            return count > 0
-
-        finally:
-            if conn.closed:
-                conn.close()
+		finally:
+			if not conn.closed:
+				conn.close()
--- sample/example_task.py
+++ sample/example_task.py
@@ -1,39 +1,77 @@
 import datetime
-
 import luigi
-from luigi.contrib import postgres
+from sqlalchemy import create_engine
+
+from postgres_target import PostgresCountTarget
+
+postgres_engine = None
+
+
+def create_postgres_engine():
+	global postgres_engine
+	postgres_engine = create_engine("postgresql://postgres:senha@localhost/postgres", pool_size=20)
 
 
 class ExampleTask(luigi.Task):
-    date = luigi.DateParameter()
+	date = luigi.DateParameter()
+	force = luigi.BoolParameter(significant=False, default=False)
 
-    def run(self):
-        print("1234")
-        print(self.date)
+	def __init__(self, *args, **kwargs):
 
-    def output(self):
-        return postgres.PostgresTarget(
-            host="localhost:5432", database="postgres", user="postgres", password="senha",
-            table="temp", update_id=str(self.date))
+		super().__init__(*args, **kwargs)
+		if self.force:
+			self.clean()
+
+	def clean(self):
+		conn = postgres_engine.connect()
+
+		try:
+			sql = "DELETE FROM {} {}".format(
+				"tabela_exemplo", "where data = '{}'".format(self.date))
+			conn.execute(sql)
+
+		finally:
+			if not conn.closed:
+				conn.close()
+
+	def run(self):
+		conn = postgres_engine.connect()
+
+		try:
+			conn.execute("INSERT INTO tabela_exemplo VALUES ('{}', '{}')".format(self.date, "texto"))
+		finally:
+			if not conn.closed:
+				conn.close()
+
+	def output(self):
+		return PostgresCountTarget(
+			sqlalchemy_engine=postgres_engine,
+			table_name="tabela_exemplo",
+			sql_filter="where data = '{}'".format(self.date)
+		)
 
 
 class ExampleAllYear(luigi.Task):
-    year = luigi.IntParameter()
-    task = luigi.TaskParameter()
+	year = luigi.IntParameter()
+	task = luigi.TaskParameter()
 
-    def requires(self):
-        date1 = datetime.date(2017, 1, 1)
-        date2 = datetime.date(2017, 12, 31)
-        day = datetime.timedelta(days=1)
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		create_postgres_engine()
 
-        while date1 <= date2:
-            date1 = date1 + day
+	def requires(self):
+		date1 = datetime.date(2017, 1, 1)
+		date2 = datetime.date(2017, 12, 31)
+		day = datetime.timedelta(days=1)
 
-            # noinspection PyCallingNonCallable
-            yield self.task(date=date1)
+		while date1 <= date2:
+			date1 = date1 + day
+
+			# noinspection PyCallingNonCallable
+			yield self.task(date=date1, force=True)
 
 
 if __name__ == '__main__':
-    luigi.run(
-        cmdline_args=["--year", "2017", "--task", "ExampleTask"], main_task_cls=ExampleAllYear,
-        local_scheduler=True)
+	luigi.run(
+		cmdline_args=["--year", "2017", "--task", "ExampleTask"], main_task_cls=ExampleAllYear,
+		local_scheduler=True)
